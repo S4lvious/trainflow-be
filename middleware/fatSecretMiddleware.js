154fc2a79ad2d0ece65f3c7194dbfeff6e1058c2
Salvatore Liccardo <s.liccardo022@gmail.com>
2024-06-07 15:32:19 +0200

    calories track

---
+++ middleware/fatSecretMiddleware.js
@@ -0,0 +1,84 @@
+const env = require('dotenv').config();
+const e = require('express');
+const connect = require('../db');
+const axios = require('axios');
+const qs = require('qs');
+
+
+const checkFatSecretExpirationToken = async (req, res, next) => {
+    const userId = req.user.id;
+    const query = 'SELECT * FROM fat_secret_user_token WHERE user_id = ?';
+    connect.query(query, [userId], (err, result) => {
+        if (err || result.length === 0) {
+            res.status(500).json({
+                error: 'Errore durante il recupero del token'
+            });
+            return;
+        } else {
+            const token = result[0].token;
+            const createdAt = result[0].created_at.toString();
+            console.log(createdAt)
+            if (isTokenExpired(createdAt) || result.length === 0) {
+                const clientId = process.env.FAT_SECRET_CLIENT_ID;
+                const clientSecret = process.env.FAT_SECRET_CLIENT_SECRET;
+                requestAccessToken(clientId, clientSecret)
+                    .then(tokenFatSecret => {
+                        const query = 'UPDATE fat_secret_user_token SET token = ?, expires_in = ? WHERE user_id = ?';
+                        connect.query(query, [tokenFatSecret.access_token, tokenFatSecret.expires_in, userId], (err, result) => {
+                            if (err) {
+                                res.status(500).json({
+                                    error: 'Errore durante la registrazione'
+                                });
+                                return;
+                            }
+                            next();
+                        });
+                    });
+            } else {
+                console.log('Token valido');
+                next();
+            }
+        }
+    });
+}
+
+async function requestAccessToken(clientId, clientSecret) {
+    try {
+      // Costruisci i dati della richiesta di token
+      const data = qs.stringify({
+        grant_type: 'client_credentials',
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: ['basic', 'premier']
+      });
+  
+      // Effettua la richiesta POST per ottenere il token di accesso
+      const response = await axios.post('https://oauth.fatsecret.com/connect/token', data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+  
+      // Restituisci il token di accesso dalla risposta
+      return response.data;
+    } catch (error) {
+      // Gestisci eventuali errori nella richiesta del token di accesso
+      throw new Error(`Errore nella richiesta del token di accesso: ${error}`);
+    }
+  }
+
+module.exports = checkFatSecretExpirationToken;
+
+
+function isTokenExpired(createdAt) {
+    // Converti il timestamp in un oggetto Date
+    const createdAtDate = new Date(createdAt);
+    if (isNaN(createdAtDate.getTime())) {
+        throw new TypeError('Il timestamp fornito non è valido');
+    }
+
+    const createdAtTimestamp = createdAtDate.getTime();
+    const twentyFourHoursInMilliseconds = 24 * 60 * 60 * 1000;
+    const currentTimestamp = Date.now(); // Ottieni l'attuale timestamp in millisecondi
+    return currentTimestamp > (createdAtTimestamp + twentyFourHoursInMilliseconds);
+}
--- routes/food.js
+++ routes/food.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const foodController = require('../controllers/foodController');
 const authenticateJWT = require('../middleware/authMiddleware');
+const checkFatSecretExpirationToken = require('../middleware/fatSecretMiddleware');
 
-router.get('/searchFood/:foodName/:userId', authenticateJWT, foodController.searchFood);
-router.get('/getFoodById/:foodId/:userId', authenticateJWT, foodController.getFoodById);
-router.get('/getFoodsByUser/:userId/:date', authenticateJWT, foodController.getFoodByUser);
-router.post('/addFoodToUser', authenticateJWT, foodController.addFood);
-router.delete('/deleteFoodFromUser/:id', authenticateJWT, foodController.deleteFood);
+router.get('/searchFood/:foodName/:userId', authenticateJWT, checkFatSecretExpirationToken, foodController.searchFood);
+router.get('/getFoodById/:foodId/:userId', authenticateJWT, checkFatSecretExpirationToken, foodController.getFoodById);
+router.get('/getFoodsByUser/:userId/:date', authenticateJWT, checkFatSecretExpirationToken, foodController.getFoodByUser);
+router.post('/addFoodToUser', authenticateJWT, checkFatSecretExpirationToken, foodController.addFood);
+router.delete('/deleteFoodFromUser/:id', authenticateJWT, checkFatSecretExpirationToken, foodController.deleteFood);
 
 module.exports = router;
